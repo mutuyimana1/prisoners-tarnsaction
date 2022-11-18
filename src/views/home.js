a7238080cade076e45ce6bf0210f35f1de1a7170
mutuyimana1 <amutuyimana64@gmail.com>
2022-11-18 10:19:12 +0200

    routing

--- src/views/home.js
+++ src/views/home.js
@@ -6,11 +6,22 @@ function Home() {
       <h3>Routing URL are as follows</h3>
 
       <h2>/</h2>
-      <h2>/Login</h2>
-      <h2>/register</h2>
-      <h2>/dashboard</h2>
-      <h2>/allprisoners</h2>
-      <h2>/registerprisoner</h2>
+      <h2>
+        <a href="/login">/Login</a>{" "}
+      </h2>
+      <h2>
+        <a href="/register">/register</a>{" "}
+      </h2>
+      <h2>
+        <a href="/dashboard">/dashboard</a>{" "}
+      </h2>
+      <h2>
+        {" "}
+        <a href="/allprisoner">/allprisoners</a>
+      </h2>
+      <h2>
+        <a href="/registerprisoner"></a>{" "}
+      </h2>
     </div>
   );
 }
