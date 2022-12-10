285ae8d1056739c8c7ec55a78ed8c351545d068d
mutuyimana1 <amutuyimana64@gmail.com>
2022-12-11 00:08:44 +0200

    home

--- src/views/home.js
+++ src/views/home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./home.css";
+import MessageIcon from "@mui/icons-material/Message";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 import money from "../assets/images/pexels-cottonbro-studio-5909808.jpg";
+import message from "../assets/images/undraw_Modern_life_re_8pdp.png";
+import undraw from "../assets/images/undraw_Credit_card_payment_re_o911.png";
 function Home() {
   return (
     <div className="home-container">
@@ -14,7 +18,7 @@ function Home() {
             </div>
             <ul>
               <li>
-                <a href="/">Home</a>{" "}
+                <a href="/">Home</a>
               </li>
               <li>
                 <a href="/sendmoney">MakeTransaction</a>
@@ -35,14 +39,19 @@ function Home() {
         </div>
       </header>
       <div className="home-background">
-        <div className="background-content">
-          <h2>
-            Make your Transaction & it will be derivered to your inmate very
-            soon as possible
-          </h2>
-          <div className="buttons">
-            <button className="button btn">How is it done</button>
-            <button className="button btn"> Make Transaction</button>
+        <div className="home-opacity">
+          <div className="background-content">
+            <h2>
+              Make your Transaction & it will be derivered to your inmate very
+              soon as possible
+            </h2>
+            <div className="buttons">
+              <button className="button btn">How is it done</button>
+              <button className="button btn"> Make Transaction</button>
+            </div>
+          </div>
+          <div className="home-text">
+            <h1>Let us support our Inmates</h1>
           </div>
         </div>
       </div>
@@ -75,19 +84,75 @@ function Home() {
             <p>card oneasdDsdvasfvfvfvffvv</p>
           </div>
         </div>
-
         <div className="home-services">
-          <div className="servicesCardOne">
-
-          </div>
-          <div className="servicesCardTwo">
-
-          </div>
-
+          <div className="servicesCardOne"></div>
+          <div className="servicesCardTwo"></div>
         </div>
       </main>
 
-      <div style={{ height: "200px" }}></div>
+      <div className="About-content">
+        <div className="About-image">
+          <img src={undraw} />
+        </div>
+        <div className="About-text">
+          <h2>
+            <b>
+              We provide a solution through quick and efficiency transaction
+            </b>
+          </h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting{" "}
+            <br />
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            <br />
+            ever since the 1500s, when an unknown printer took a galley of type
+            <br />
+            and scrambled it to make a type specimen book. It has survived not
+            <br />
+            only five centuries, but also the leap into electronic typesetting,
+            <br />
+            remaining essentially unchanged
+          </p>
+        </div>
+      </div>
+
+      <div className="home-contact">
+        <div style={{ width: "50%" }}>
+          <h1>Chart with us</h1>
+          <h1>
+            <b>Let's talk About our product to you!!</b>
+          </h1>
+        </div>
+        <div style={{ padding: "50px 70px", display: "flex" }}>
+          <div>
+            <button
+              style={{
+                marginTop: "100px",
+                width: "200px",
+                padding: "10px",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              {" "}
+              Contact with Us
+              <ArrowCircleRightIcon style={{ marginLeft: "5px" }} />
+            </button>
+          </div>
+
+          <div>
+            <img src={message} style={{ width: "400px" }} />
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <div>
+          <h1>Logo</h1>
+        </div>
+        <div>
+          <MessageIcon /> <ArrowCircleRightIcon />
+        </div>
+      </div>
     </div>
   );
 }
